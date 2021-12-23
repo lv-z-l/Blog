2a58a7f830d3b739c8fb4f8062476405a2d01ea8
lvzl <lvzl@yusys.com.cn>
2021-12-23 21:51:32 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    title: 'docs',
+    title: 'Blog',
     description: '啊！朕学不动了',
     port: 6600,
-    base: '/docs/',
+    base: '/blog/',
     theme: 'reco',
     themeConfig: {
         nav: [
@@ -110,7 +110,7 @@ module.exports = {
         },
         lastUpdated: 'Last Updated', // string | boolean
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'docs',
+        repo: 'blog',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: '查看源码',
@@ -118,9 +118,9 @@ module.exports = {
         // 以下为可选的编辑链接选项
 
         // 假如你的文档仓库和项目本身不在一个仓库：
-        docsRepo: 'docs',
+        docsRepo: 'blog',
         // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
+        docsDir: 'docs/.vuepress/dist',
         // 假如文档放在一个特定的分支下：
         docsBranch: 'master',
         // 默认是 false, 设置为 true 来启用
